db682bd7b5742ab13ee22095f2044c9008e1b146
Василь Боднарук <vasylbodnaruk@gmail.com>
2016-08-02 16:29:22 +0300

    First commit

---
+++ assets/js/global/menu.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+  $(function () {
+    var pull = $('#pull');
+    menu = $('nav ul');
+    menuHeight = menu.height();
+    $(pull).on('click', function (e) {
+      e.preventDefault();
+      menu.slideToggle();
+    });
+
+    $(window).resize(function () {
+      var w = $(window).width();
+      if (w > 970 && menu.is(':hidden')) {
+        menu.removeAttr('style');
+      }
+    });
+  });
+});
