9598703600df031150bace5d3235f79b2075e81c
Василь Боднарук <vasylbodnaruk@gmail.com>
2016-08-08 16:43:59 +0300

    Footer

--- assets/js/global/main.js
+++ assets/js/global/main.js
@@ -2,3 +2,11 @@ $(document).ready(function () {
   $(".right_content").stick_in_parent({
   });
 });
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "//connect.facebook.net/uk_UA/sdk.js#xfbml=1&version=v2.7&appId=1715014728749508";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
